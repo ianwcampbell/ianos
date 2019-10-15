39c0ca2b8e05fd43f4cd4099a9c142c4564867ef
Ian <ixc9335@rit.edu>
2019-10-15 14:13:12 -0700

    initial commit

---
+++ src/kernel/kernel.c
@@ -0,0 +1,27 @@
+#include <stddef.h>
+#include <stdint.h>
+#include <kernel/uart.h>
+#include <kernel/mem.h>
+#include <kernel/atag.h>
+#include <common/stdio.h>
+#include <common/stdlib.h>
+
+void kernel_main(uint32_t r0, uint32_t r1, uint32_t atags)
+{
+    char buf[256];
+    // Declare as unused
+    (void) r0;
+    (void) r1;
+    (void) atags;
+
+    uart_init();
+    puts("Initializing Memory Module\n");
+    mem_init((atag_t *)atags);
+
+    puts("Hello, kernel World!\n");
+    while (1) {
+        gets(buf,256);
+        puts(buf);
+        putc('\n');
+    }
+}
